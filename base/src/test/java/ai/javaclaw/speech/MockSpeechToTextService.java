16edc98c127889551808a6d8d96bb916bfcea287
SoufianeBouaddis <fous.enaibdisbdis@gmail.com>
2026-04-27 20:16:32 +0100

    Move OpenAiSpeechToTextService.java to base/src/test/java/ai/javaclaw/speech/MockSpeechToTextService and refactor OpenAiSpeechToTextService to deletate it to SpringAI

--- base/src/test/java/ai/javaclaw/speech/MockSpeechToTextService.java
+++ base/src/test/java/ai/javaclaw/speech/MockSpeechToTextService.java
@@ -1,12 +1,7 @@
 package ai.javaclaw.speech;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.stereotype.Service;
-
 import java.io.InputStream;
 
-@Service
-@ConditionalOnProperty(name = "speech.provider", havingValue = "mock", matchIfMissing = true)
 public class MockSpeechToTextService implements SpeechToTextService {
 
     @Override
